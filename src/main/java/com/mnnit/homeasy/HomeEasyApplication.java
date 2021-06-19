80a528b14aaaff27673e29d14212dc8640fc054b
vishalzen <vishal.vegeta@gmail.com>
2021-06-19 14:51:01 +0530

    basic controller

---
+++ src/main/java/com/mnnit/homeasy/HomeEasyApplication.java
@@ -0,0 +1,12 @@
+package com.mnnit.homeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeEasyApplication.class, args);
+    }
+}
